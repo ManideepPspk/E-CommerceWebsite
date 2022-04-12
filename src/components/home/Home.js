a5e6dabe037da37c45fbf2b5c9cd1e9962192db6
MANIDEEP GODAVARTHY <manideepgodavarthy@gmail.com>
2022-04-12 18:59:26 +0530

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="jumbotron">
+                    <h1 className="display-4">Hello All!</h1>
+                    <p className="">This is a basic E commerce app build using react without any backend (just hardcoded in frontend)</p>
+                <hr className="my-4" />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
